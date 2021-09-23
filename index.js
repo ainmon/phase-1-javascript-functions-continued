45f834201cc0faa0362d172af179b1848838467a
Adam <inmon.adam@yahoo.com>
2021-09-22 22:18:44 -0500

    commit tree 9f86c3a4a60cec17950069058357b16a44bfdae5

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Your code here
+function saturdayFun(act = 'roller-skate'){
+    return `This Saturday, I want to ${act}!`;
+}
+
+const mondayWork = function (act = 'go to the office'){
+    return `This Monday, I will ${act}.`;
+}
+
+function wrapAdjective(wow = '*'){
+    const innerFunction = function (){
+        return function (msg = 'special'){
+            return `You are ${wow}${msg}${wow}!`;
+        }
+    }
+    return innerFunction();
+}
+
+
+
+
+
+
